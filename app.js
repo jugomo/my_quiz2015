8bf88ff4dcf461f30494281d499ca6c236c8b48f
jugomo <julio@MacBook-de-julio.local>
2015-07-08 20:44:02 +0200

    Primera página y favicon

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// importar paquetes
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,26 +6,29 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//importar enrutadores
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
+// crear aplicacion
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// instalar middlewares
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// instalar enrutadores
 app.use('/', routes);
-app.use('/users', users);
 
+// resto de rutas: generar error 404
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -56,5 +60,5 @@ app.use(function(err, req, res, next) {
     });
 });
 
-
+// exportar app para comando de arranque
 module.exports = app;
