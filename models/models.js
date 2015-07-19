4e5754072a16b9ba507824f84c8797ca1f6718e7
jugomo <julio@MacBook-de-julio.local>
2015-07-19 18:54:00 +0200

    Paso 8: Desplegar en Heroku con Postgress

--- models/models.js
+++ models/models.js
@@ -2,8 +2,37 @@
 
 var path = require('path');
 
+// URL para produccion (heroku):
+// Postges DATABASE_URL = postgres://user:passwd@host:port/database
+
+// URL para local:
+// SQLite  DATABASE_URL = sqlite://:@:/
+
+// extraigo los parametros de conexion a la BD de la url con reg-exp
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6] || null);
+var user     = (url[2] || null);
+var pwd      = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect  = (url[1] || null);
+var port     = (url[5] || null);
+var host     = (url[4] || null);
+var storage  = process.env.DATABASE_STORAGE;
+
 // cargar modelo ORM
 var Sequelize = require('sequelize');
+
+// Cargar Modelo ORM
+var sequelize = new Sequelize(DB_name, user, pwd,
+  {
+    dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postges
+  }
+);
 
 // usar BBDD SQLite:
 var sequelize = new Sequelize(null, null, null,
