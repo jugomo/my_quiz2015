679a25d99909ca211a6149b08bc1e5b18935193e
jugomo <julio@MacBook-de-julio.local>
2015-07-21 20:57:28 +0200

    Paso 10: Autoload de la BD

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,32 +1,40 @@
 // importar el modelo para poder acceder a la BD
 var models = require('../models/models.js');
 
-// GET /quizes
+// Autoload - factoriza el código si ruta incluye .quizId
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(function(quiz) {
+    if (quiz) {
+      req.quiz = quiz;
+      next();
+    } else {
+      next(new Error('No existe quizId=' + quizId));
+    }
+  }).catch(function(error) { next(error); });
+};
+
+// GET /quizes (con gestion de errores)
 exports.index = function(req, res) {
   models.Quiz.findAll().then(function(quizes) {
     res.render('quizes/index.ejs', { quizes: quizes });
-  })
-}
+  }).catch(function(error) { next(error); })
+};
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz){
-    res.render('quizes/show', { quiz: quiz });
-  })
+  res.render('quizes/show', { quiz: req.quiz });
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
-    if (req.query.respuesta === quiz.respuesta) {
-      res.render('quizes/answer', { quiz: quiz, respuesta: 'Correcto' });
-    } else {
-      res.render('quizes/answer', { quiz: quiz, respuesta: 'Incorrexto' });
-    }
-  })
+  var resultado = 'Incorrexto';
+  if (req.query.respuesta === req.quiz.respuesta) {
+    resultado = 'Correcto';
+  }
+  res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado });
 };
 
 // GET /author
 exports.author = function(req, res) {
   res.render('author', {  });
-}
+};
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,12 @@ router.get('/', function(req, res) {
 // GET author page
 router.get('/author', quizController.author);
 
+// Autoload de comandos con :quizId
+// se instala para que se ejecute antes que lo necesiten las rutas
+// show y answer y solo en el caso de que path contenga :quizId en
+// algun lugar (query, body, param) de la cabecera HTTP
+router.param('quizId', quizController.load);
+
 // GET questions page
 router.get('/quizes',               quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
